426eed01889686aa255d9d48d8426966087db2c8
fathulj <92386962+fathulj@users.noreply.github.com>
2021-12-01 11:25:47 +0800

    perubahan 1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
+    testingg
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
